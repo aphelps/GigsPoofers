ad2ad7960404d03836f044534a50b0877c80f675
Adam Phelps <amp@cs.stanford.edu>
2012-07-02 02:57:48 -0700

    * Added timeouts to all actions   - Need to make separate classes for ignitors and solenoids

--- brain_v1/brain_actions.ino
+++ brain_v1/brain_actions.ino
@@ -1,28 +1,88 @@
+#if 0
 void switch_solenoid(int pin, int value, void *arg)
 {
-  int led_pin = (int)arg;
-  digitalWrite(led_pin, value);
+  Poofer *poofer = (Poofer *)arg;
+  digitalWrite(poofer->sol_led, value);
+  digitalWrite(poofer->sol_relay, value);
   DEBUG_PRINT(2, "switch_solenoid pin=");
   DEBUG_PRINT(2, pin);
-  DEBUG_PRINT(2, " led_pin=");
-  DEBUG_PRINT(2, led_pin);
+  DEBUG_PRINT(2, " sol_relay=");
+  DEBUG_PRINT(2, poofer->sol_relay);
+  DEBUG_PRINT(2, " sol_led=");
+  DEBUG_PRINT(2, poofer->sol_led);
   DEBUG_PRINT(2, " value=");
   DEBUG_PRINT(2, value);
   DEBUG_PRINT(2, "\n");
 }
+#endif
+void switch_solenoid(int pin, int value, void *arg)
+{
+  Poofer *poofer = (Poofer *)arg;
+
+  poofer->sol_value = 0;
+  if (value) {
+    if ((poofer->sol_turned_on_ms == 0) ||
+      millis() < poofer->sol_turned_on_ms + SOLENOID_TIMEOUT_MS) {
+      if (poofer->sol_turned_on_ms == 0)
+        poofer->sol_turned_on_ms = millis();
+      poofer->sol_value = 1;
+    }
+  } else {
+    poofer->sol_turned_on_ms = 0;
+  }
+
+  digitalWrite(poofer->sol_led, poofer->sol_value);
+  digitalWrite(poofer->sol_relay, poofer->sol_value);
+  DEBUG_PRINT(2, "switch_solenoid pin=");
+  DEBUG_PRINT(2, pin);
+  DEBUG_PRINT(2, " value=");
+  DEBUG_PRINT(2, value);
+  DEBUG_PRINT(2, " sol_relay=");
+  DEBUG_PRINT(2, poofer->sol_relay);
+  DEBUG_PRINT(2, " sol_led=");
+  DEBUG_PRINT(2, poofer->sol_led);
+  DEBUG_PRINT(2, " sol_value=");
+  DEBUG_PRINT(2, poofer->sol_value);
+  DEBUG_PRINT(2, " sol_turned_on_ms=");
+  DEBUG_PRINT(2, poofer->sol_turned_on_ms);
+  DEBUG_PRINT(2, "\n");
+
+}
+
 
 
 void switch_igniter(int pin, int value, void *arg)
 {
-  int led_pin = (int)arg;
-  digitalWrite(led_pin, value);
+  Poofer *poofer = (Poofer *)arg;
+
+  poofer->ign_value = 0;
+  if (value) {
+    if ((poofer->ign_turned_on_ms == 0) ||
+      millis() < poofer->ign_turned_on_ms + IGNITION_TIMEOUT_MS) {
+      if (poofer->ign_turned_on_ms == 0)
+        poofer->ign_turned_on_ms = millis();
+      poofer->ign_value = 1;
+    }
+  } else {
+    poofer->ign_turned_on_ms = 0;
+  }
+
+  digitalWrite(poofer->ign_led, poofer->ign_value);
+  digitalWrite(poofer->ign_relay, poofer->ign_value);
   DEBUG_PRINT(2, "switch_igniter pin=");
   DEBUG_PRINT(2, pin);
-  DEBUG_PRINT(2, " led_pin=");
-  DEBUG_PRINT(2, led_pin);
   DEBUG_PRINT(2, " value=");
   DEBUG_PRINT(2, value);
+  DEBUG_PRINT(2, " ign_relay=");
+  DEBUG_PRINT(2, poofer->ign_relay);
+  DEBUG_PRINT(2, " ign_led=");
+  DEBUG_PRINT(2, poofer->ign_led);
+  DEBUG_PRINT(2, " ign_value=");
+  DEBUG_PRINT(2, poofer->ign_value);
+  DEBUG_PRINT(2, " ign_turned_on_ms=");
+  DEBUG_PRINT(2, poofer->ign_turned_on_ms);
   DEBUG_PRINT(2, "\n");
+
 }
 
 
--- brain_v1/brain_v1.ino
+++ brain_v1/brain_v1.ino
@@ -2,26 +2,50 @@
 #include "Debug.h"
 
 /* Relay pins */
-#define SOLENOID_1_RELAY_PIN 4
-#define IGNITER_1_RELAY_PIN 3
-#define SOLENOID_2_RELAY_PIN 2
-#define IGNITER_2_RELAY_PIN 1
+#define SOLENOID_1_RELAY_PIN 13
+#define IGNITER_1_RELAY_PIN 2
+#define SOLENOID_2_RELAY_PIN 3
+#define IGNITER_2_RELAY_PIN 4
 
 /* Arduino pins */
 int SOLENOID_1_LED_PIN = 10;
 int IGNITER_1_LED_PIN = 9;
+int SOLENOID_1_SWITCH_PIN = 7;
+int IGNITER_1_SWITCH_PIN = 8;
+
 int SOLENOID_2_LED_PIN = 6;
 int IGNITER_2_LED_PIN = 5;
+int SOLENOID_2_SWITCH_PIN = 11;
+int IGNITER_2_SWITCH_PIN = 12;
 
-int SOLENOID_1_SWITCH_PIN = 13;
-int IGNITER_1_SWITCH_PIN = 2;
-int SOLENOID_2_SWITCH_PIN = 3;
-int INGNITER_2_SWITCH_PIN = 4;
+Poofer poofer_1(
+  SOLENOID_1_SWITCH_PIN,
+  SOLENOID_1_LED_PIN,
+  SOLENOID_1_RELAY_PIN,
 
-Switch poofer_solenoid_1(7, false, switch_solenoid, (void *)SOLENOID_1_LED_PIN);
-Switch poofer_igniter_1(8,  false, switch_igniter,  (void *)IGNITER_1_LED_PIN);
-Switch poofer_solenoid_2(11,  false, switch_solenoid, (void *)SOLENOID_2_LED_PIN);
-Switch poofer_igniter_2(12,   false, switch_igniter,  (void *)IGNITER_2_LED_PIN);
+  IGNITER_1_SWITCH_PIN,  
+  IGNITER_1_LED_PIN,
+  IGNITER_1_RELAY_PIN
+  );
+
+Poofer poofer_2(
+  SOLENOID_2_SWITCH_PIN,
+  SOLENOID_2_LED_PIN,
+  SOLENOID_2_RELAY_PIN,
+
+  IGNITER_2_SWITCH_PIN,  
+  IGNITER_2_LED_PIN,
+  IGNITER_2_RELAY_PIN);
+
+
+Switch poofer_solenoid_1(SOLENOID_1_SWITCH_PIN, false,
+                         switch_solenoid, (void *)&poofer_1);
+Switch poofer_igniter_1(IGNITER_1_SWITCH_PIN,  false,
+                        switch_igniter,  (void *)&poofer_1);
+Switch poofer_solenoid_2(SOLENOID_2_SWITCH_PIN,  false,
+                         switch_solenoid, (void *)&poofer_2);
+Switch poofer_igniter_2(IGNITER_2_SWITCH_PIN,   false,
+                        switch_igniter,  (void *)&poofer_2);
 
 #define NUM_PINS 21
 Pin *pinArray[NUM_PINS] = {
@@ -66,6 +90,6 @@ void loop() {
   /* Check switches and perform actions */
   checkSwitches(pinArray, NUM_PINS, true);
 
-  delay(10);
+//  delay(100);
 }
 
