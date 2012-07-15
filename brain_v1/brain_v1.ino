762a73083a2c8f711bac0d45b5d9bff69f1cc1b0
Adam Phelps <amp@cs.stanford.edu>
2012-07-14 23:06:33 -0700

    * Initial attempt at incorporating JE's XBee code

--- brain_v1/brain_v1.ino
+++ brain_v1/brain_v1.ino
@@ -1,5 +1,10 @@
 #include "Pins.h"
 #include "Debug.h"
+#include "State.h"
+
+/*
+  A - igniter 1B - igniter 2C - poofer 1D - poofer 2
+*/
 
 /* Relay pins */
 #define SOLENOID_1_RELAY_PIN 5
@@ -18,6 +23,7 @@ int IGNITER_2_LED_PIN = 13;
 int SOLENOID_2_SWITCH_PIN = 11;
 int IGNITER_2_SWITCH_PIN = 12;
 
+#define NUM_POOFERS 2
 Poofer poofer_1(
   SOLENOID_1_SWITCH_PIN,
   SOLENOID_1_LED_PIN,
@@ -87,6 +93,44 @@ void loop() {
   /* Check switches and perform actions */
   checkSwitches(pinArray, NUM_PINS, true);
 
+  int state[2];
+  
+  while (Serial.available()) {
+    char value = Serial.read();
+    if ((value >= BASE_CHAR) && (value < BASE_CHAR + NUM_POOFERS)) {
+      int index = value - BASE_CHAR;
+      /* Ignition */
+      state[index] |= IGN_STATE;
+    } else if ((value >= BASE_CHAR + NUM_POOFERS) && (value < BASE_CHAR + (2 * NUM_POOFERS))) {
+      int index = value - (BASE_CHAR + NUM_POOFERS);
+      /* Poof on */
+      state[index] |= POOF_STATE;
+    } else if (value == DELIMITER) {
+      for (int i = 0; i < NUM_POOFERS; i++ ) {
+//        _poofers[i] = _newPoofers[i];
+//        _newPoofers[i] = 0;
+        if (i == 0) {
+          switch_igniter(-1, state[i] & IGN_STATE, &poofer_1);
+        } else {
+          switch_igniter(-1, state[i] & IGN_STATE, &poofer_2);
+        }
+
+        if (i == 0) {
+          switch_igniter(-1, state[i] & POOF_STATE, &poofer_1);
+        } else {
+          switch_igniter(-1, state[i] & POOF_STATE, &poofer_2);
+        }
+        state[i] = 0;
+      }
+    } else {
+      for (int i = 0; i < NUM_POOFERS; i++) {
+//        _newPoofers[i] = 0;
+        state[i] = 0;
+      }
+    }
+
+  }
+  
 //  delay(100);
 }
 
