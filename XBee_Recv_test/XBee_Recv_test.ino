ae2501a6e0287b6b058c9366d01364b11e84c497
Adam Phelps <amp@cs.stanford.edu>
2012-07-23 19:04:59 -0700

    * Added flashing debug LED to the Recv test

--- XBee_Recv_test/XBee_Recv_test.ino
+++ XBee_Recv_test/XBee_Recv_test.ino
@@ -1,11 +1,14 @@
-#define RECV_LED 4
+#define RECV_LED 8
+#define DEBUG_LED 12
 
 void setup() {
-  pinMode(RECV_LED, OUTPUT);  
+  pinMode(RECV_LED, OUTPUT);
+  pinMode(DEBUG_LED, OUTPUT);
   Serial.begin(9600);
 }
 
 int ontime = 0;
+int debug_time = 0;
 
 void loop() {
   char value;
@@ -21,5 +24,15 @@ void loop() {
     if (millis() - ontime > 1000) {
       digitalWrite(RECV_LED, LOW);
     } 
+  }
+
+  int elapsed = millis() - debug_time;
+  if (debug_time == 0) {
+    digitalWrite(DEBUG_LED, HIGH);
+    debug_time = millis();
+  } else if (elapsed > 1000 && elapsed < 2000) {
+    digitalWrite(DEBUG_LED, LOW);
+  } else if (elapsed > 2000) {
+    elapsed = 0;
   }
 }
