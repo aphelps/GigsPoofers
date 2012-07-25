d54361eda5e12d34582fb3a2990a8d99b0b37d62
Adam Phelps <amp@cs.stanford.edu>
2012-07-25 00:04:23 -0700

    * Changed test control code to alternate between ign, poof, off

--- control_floyd/control_floyd.ino
+++ control_floyd/control_floyd.ino
@@ -1,3 +1,6 @@
+//#define DEBUG
+//#define DEBUG_VERBOSE 2
+
 #include "Debug.h"
 #include "State.h"
 
@@ -11,20 +14,28 @@ void setup() {
 }
 
 
-unsigned int ontime = 0;
+unsigned int ontime = millis();
+
+#define NUM_MODES 3
+unsigned int mode = 0;
 
 void loop() {
+  mode = (millis() / 1024) % NUM_MODES;
+
   for (int i = 0; i < NUM_POOFERS; i++) {
-    if ((ontime == 0) || ((millis() - ontime) < 1000)) {
-      state.set_ign(0, true);
-      state.set_poof(0, true);
-      if (ontime == 0) ontime = millis();
-    } else {
-      state.set_ign(0, false);
-      state.set_poof(0, false);
-      if (millis() - ontime > 2000) {
-        ontime = 0;
-      }
+    switch (mode) {
+        case 0:
+          if (!state.get_ign(i)) state.set_ign(i, true);
+          if (state.get_poof(i)) state.set_poof(i, false);
+          break;
+        case 1:
+          if (state.get_ign(i)) state.set_ign(i, false);
+          if (!state.get_poof(i)) state.set_poof(i, true);
+          break;
+        case 2:
+          if (state.get_ign(i)) state.set_ign(i, false);
+          if (state.get_poof(i)) state.set_poof(i, false);
+          break;
     }
   }
   
